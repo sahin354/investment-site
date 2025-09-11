0d4f192ab6c8f480bdeb82f1bdeeee3bd93418b5
sahin354 <aivlog59952@gmail.com>
2025-09-11 02:58:50 -0700

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -1,9 +1,12 @@
-// --- PASTE YOUR FIREBASE CONFIG OBJECT HERE ---
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    // ...etc
+  apiKey: "AIzaSyBqnJpGCtplUIwspovyntn9bbaTY2ygLNE",
+  authDomain: "adani-investment.firebaseapp.com",
+  projectId: "adani-investment",
+  storageBucket: "adani-investment.firebasestorage.app",
+  messagingSenderId: "549652082720",
+  appId: "1:549652082720:web:09bc0f371a498ee5184c45",
+  measurementId: "G-TGFHW9XKF2"
 };
 
 // Initialize Firebase
