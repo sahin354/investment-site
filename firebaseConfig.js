464d79d8adc8c00ed504f084eec755e74c6b3727
sahin354 <aivlog59952@gmail.com>
2025-09-16 00:50:59 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqnJpGCtplUIwspovyntn9bbaTY2ygLNE",
+  authDomain: "adani-investment.firebaseapp.com",
+  projectId: "adani-investment",
+  storageBucket: "adani-investment.firebasestorage.app",
+  messagingSenderId: "549652082720",
+  appId: "1:549652082720:web:09bc0f371a498ee5184c45",
+  measurementId: "G-TGFHW9XKF2"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
