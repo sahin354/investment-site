d3717493bf21588b4a80af303d6789ea00cb7b5e
sahin354 <aivlog59952@gmail.com>
2025-11-16 19:03:52 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,3 @@
-// firebaseConfig.js
-
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getFunctions, httpsCallable } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBqnJpGCtplUIwspovyntn9bbaTY2ygLNE",
@@ -14,17 +8,3 @@ const firebaseConfig = {
   appId: "1:549652082720:web:09bc0f371a498ee5184c45",
   // measurementId: "G-XXXXXXXXXX"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const functions = getFunctions(app);
-
-export { 
-  app, 
-  db, 
-  auth, 
-  functions, 
-  httpsCallable 
-};
-
