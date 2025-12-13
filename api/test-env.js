4c2686b820ca7bb40b2b7c027be9687256070b0b
sahin354 <aivlog59952@gmail.com>
2025-12-13 23:15:06 +0530

    Delete api/test-env.js

--- api/test-env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    PAY0_CREATE_URL: process.env.PAY0_CREATE_URL ? "OK" : "MISSING",
-    PAY0_USER_TOKEN: process.env.PAY0_USER_TOKEN ? "OK" : "MISSING",
-    PAY0_REDIRECT_URL: process.env.PAY0_REDIRECT_URL ? "OK" : "MISSING"
-  });
-}
